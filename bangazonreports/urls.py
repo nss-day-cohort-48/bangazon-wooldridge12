4f7577a0c8e025ec9a0cfc5d834477ce56f69ba2
wooldridge12 <wooldridgemikey@gmail.com>
2021-09-02 16:38:45 -0400

    report for completed orders

--- bangazonreports/urls.py
+++ bangazonreports/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from .views import favoriteseller_list
 from .views import incompleteorder_list
+from .views import completeorder_list
 
 urlpatterns = [
     path('reports/userfavorites', favoriteseller_list),
-    path('reports/incompleteorders', incompleteorder_list)
+    path('reports/incompleteorders', incompleteorder_list),
+    path('reports/completeorders', completeorder_list)
 ]
--- bangazonreports/views/__init__.py
+++ bangazonreports/views/__init__.py
@@ -1,3 +1,4 @@
 from .connection import Connection
 from .users.favoritesellers import favoriteseller_list
 from .users.incompleteorders import incompleteorder_list
+from .users.completeorders import completeorder_list
